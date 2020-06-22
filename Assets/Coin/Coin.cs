5577ffc38647e2f9ae9d502999e6d7b901cf77ba
Edwin Arley Tangarife Tobón <eatangaife@gmail.com>
2020-06-21 21:37:44 -0500

    Complete

---
+++ Assets/Coin/Coin.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+
+    //Variable Global y estatica.
+    public static int coinsCount = 0;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Juego iniciado");
+        Coin.coinsCount++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*
+     * Este metodo se llama cuando se entra en contacto con el collider del gameobject
+     * */
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Algo ocurrio con la moneda");
+            Coin.coinsCount--;
+
+            if(Coin.coinsCount == 0)
+            {
+                Debug.Log("Juego Terminado.");
+                GameObject gameManager = GameObject.Find("GameManager");
+                Destroy(gameManager);
+
+                GameObject[] fireWorkSystem = GameObject.FindGameObjectsWithTag("Fireworks");
+                foreach (GameObject fireWork in fireWorkSystem)
+                {
+                    fireWork.GetComponent<ParticleSystem>().Play();
+                }
+            }
+            Destroy(gameObject);
+        }
+        
+    }
+}
---
+++ Assets/Timer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public float maxTime = 60f;
+    private float countDown = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countDown = maxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countDown -=  Time.deltaTime;
+        //Debug.Log("Tiempo" + countDown);
+        if (countDown <= 0)
+        {
+            Debug.Log("Se acabo el tiempo");
+            Coin.coinsCount = 0;
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+}
